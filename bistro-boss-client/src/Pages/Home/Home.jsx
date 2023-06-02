89c9557c50279b7b52475b2269aec18249a71ba7
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-02 20:56:29 +0600

    Implemented title change and Created menu page
    Now the title of the website changes with moving from page to page. And also created the menu page (but did not create any contents in it yet)

--- bistro-boss-client/src/Pages/Home/Home.jsx
+++ bistro-boss-client/src/Pages/Home/Home.jsx
@@ -3,18 +3,22 @@ import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Featured from "./Featured/Featured";
 import PopularMenu from "./PopularMenu/PopularMenu";
-
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Category></Category>
-            <Featured></Featured>
-            <PopularMenu></PopularMenu>
-            <Testimonials></Testimonials>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
+
+      <Banner></Banner>
+      <Category></Category>
+      <Featured></Featured>
+      <PopularMenu></PopularMenu>
+      <Testimonials></Testimonials>
+    </div>
+  );
 };
 
 export default Home;
