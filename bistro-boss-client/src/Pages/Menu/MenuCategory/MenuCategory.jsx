1f0e634a5a27a9806c31e95b59bdfb192be88f3a
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-04 23:38:55 +0600

    Created Order Now button for menu page
    Now displaying order now buttons in menu page

--- bistro-boss-client/src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ bistro-boss-client/src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,6 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import { Link } from "react-router-dom";
 
 const MenuCategory = ({ subHeading, heading, items }) => {
   return (
@@ -12,6 +13,11 @@ const MenuCategory = ({ subHeading, heading, items }) => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <Link to='/order'>
+        <button className="btn btn-outline border-0 border-b-2 text-black mt-4">
+          Order Now!
+        </button>
+      </Link>
     </section>
   );
 };
