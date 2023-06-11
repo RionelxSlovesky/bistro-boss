8cf8e9b3e3415c7b9104d823cd5ebcf7821583a8
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-11 19:59:50 +0600

    Created get all users API
    Created API in the server side to get all the users information

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -41,6 +41,12 @@ async function run() {
 
 
     // users
+
+    app.get('/users', async(req,res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       const query = {email: user.email}
